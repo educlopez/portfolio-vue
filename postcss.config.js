18d52d41c5587652976deb18ceb02cf9def6b30f
educlopez <educlopez93@gmail.com>
2021-04-07 20:18:39 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+const purgecss = require("@fullhuman/postcss-purgecss")
 
+module.exports = {
+  plugins: [
+    //Only add purgecss in production
+
+    process.env.NODE_ENV === "production"
+      ? purgecss({
+          content: ["./src/**/*.vue"]
+        })
+      : ""
+  ]
+}
